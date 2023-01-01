329788f17d5c51657f39aa9e29c681e5bad30311
ACACIAN <gkehdrn95@netand.co.kr>
2023-01-01 13:20:58 +0900

    resource paste

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,9 +4,11 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+
         $('#btn-update').on('click', function () {
             _this.update();
         });
+
         $('#btn-delete').on('click', function () {
             _this.delete();
         });
@@ -59,7 +61,7 @@ var main = {
             type: 'DELETE',
             url: '/api/v1/posts/' + id,
             dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
+            contentType: 'application/json; charset=utf-8'
         }).done(function () {
             alert('글이 삭제되었습니다.');
             window.location.href = '/';
@@ -67,6 +69,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     }
+
 };
 
 main.init();
